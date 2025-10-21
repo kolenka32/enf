4d0beb12ca014485dce465968daaa0d8904faf93
Roman <s3698599@gmail.com>
2025-10-21 21:37:52 +0300

    add users app (forms,views,urls,models)

--- enf/urls.py
+++ enf/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('', include('main.urls', namespace='main')),
+    path('users/', include('cart.urls', namespace='users'))
 ]
 
 if settings.DEBUG:
